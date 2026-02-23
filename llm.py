e2bc0a306a1153af10bf12d5a9fd28f38c53b891
Lussandro <lussandrorj2@hotmail.com>
2026-02-23 20:32:11 -0300

    fix: Correção da função ask_llm, respondendo com erro caso tenha.

--- llm.py
+++ llm.py
@@ -12,4 +12,18 @@ def ask_llm(text: str):
         contents=text
     )
 
-    return response.text
+    return response.textdef ask_llm(text: str):
+    try:
+        client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=text
+        )
+
+        print("Resposta Gemini:", response.text)
+        return response.text
+
+    except Exception as e:
+        print("Erro Gemini:", e)
+        return "Erro interno no processamento."
