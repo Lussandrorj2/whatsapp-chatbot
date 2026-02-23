cbf52d8c6b69f7318ca1dffa466a00d10f7ab9e3
Lussandro <lussandrorj2@hotmail.com>
2026-02-23 20:49:20 -0300

    fix: correção sintax

--- llm.py
+++ llm.py
@@ -5,14 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def ask_llm(text: str):
-    client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
-
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=text
-    )
-
-    return response.textdef ask_llm(text: str):
     try:
         client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
